26812b84af0337f645e31304b967fb08df05b8bd
21121a1218 <calveenstephene@gmail.com>
2024-06-10 10:18:11 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+class Test
+{
+public static void main(String args[])
+{
+HashMap<Integer,String> caly = new HashMap<>();
+ caly.put(2,"cale");
+ caly.put(1,"alex");
+ caly.put(4,"bas");
+ caly.put(3,"dolu");
+System.out.println(caly);
+System.out.println("size" +caly.size());
+}
+}
